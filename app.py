a710843321a4ce8a29c68807f84ad1606e12ae0b
Budvin Chathura <chathura97rc@gmail.com>
2020-03-03 21:56:20 +0530

    item get post methods

--- app.py
+++ app.py
@@ -1,2 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+class Item(Resource):
+    def get(self,name):
+        item = next(filter(lambda x: x['name']==name,items),None)
+        return {'item':item}, 200 if item else 404
+
+    def post(self,name):
+        if next(filter(lambda x: x['name']==name,items),None) is not None:
+            return {'message':'An item with name {} already exists'.format(name)}, 400
+        data = request.get_json(silent=True)
+        item = {"name":name,"price":data['price']}
+        items.append(item)
+        return item, 201
+
+class ItemList(Resource):
+    def get(self):
+        return {'items':items}
+
+api.add_resource(Item,'/item/<string:name>')
+api.add_resource(ItemList,'/items')
+app.run()
