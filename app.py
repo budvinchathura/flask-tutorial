e8d69469d1dfe0739f2f6e92e71d4b2e70f1c76f
Budvin Chathura <chathura97rc@gmail.com>
2020-03-15 21:12:25 +0530

    added put method

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from flask_jwt import JWT,jwt_required
 
 from security import authenticate,identity
@@ -30,10 +30,21 @@ class Item(Resource):
         items = list(filter(lambda x: x['name']==name,items))
         return {'message':'Item deleted'}
 
+    def put(self,name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name']==name,items),None)
+        if item is None:
+            item = {'name':name,'price':data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+    
+
 class ItemList(Resource):
     def get(self):
         return {'items':items}
 
 api.add_resource(Item,'/item/<string:name>')
 api.add_resource(ItemList,'/items')
-app.run()
+app.run(debug=True)
